4dd829259234057d653d82a0dd3c5b678fdc23ec
Trevor Stahl <trevor@trevorstahl.com>
2024-01-08 18:10:09 -0600

    working on trying to revamp user model, need to think about it more

--- zip_tribe_api/app/models/user.py
+++ zip_tribe_api/app/models/user.py
@@ -7,7 +7,6 @@ class UserBase(BaseModel):
     username: str
     first_name: str
     last_name: str
-    middle_name: str = None  # Optional
     dob: datetime.date
     email: EmailStr
     phone: str
@@ -15,14 +14,29 @@ class UserBase(BaseModel):
     active: bool = True
 
 
-class UserCreate(UserBase):
+class UserSignup(UserBase):
     password: str
 
 
-class UserUpdate(UserBase):
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+
+class UserSelfUpdate(UserBase):
+    username = str
     password: Optional[str] = None
+    zipcode: Optional[int] = None
+    phone: Optional[str] = None
+    email: Optional[EmailStr] = None
+    active: Optional[bool] = None
 
 
+class UserAdminDeactivation(UserBase):
+    active: bool = False
+
+
+# TODO
 class UserInDB(UserBase):
     hashed_password: str
     is_active: bool = True
